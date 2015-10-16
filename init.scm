c59c7138e06e904472836130dfc13ffb92d56330
Nala Ginrut <nalaginrut@gmail.com>
2015-10-17 07:21:37 +0800

    added start script

---
+++ init.scm
@@ -0,0 +1,4 @@
+(use-modules (lambda-tortoise serial)
+             (lambda-tortoise instructions))
+(init-tortoise)
+(display "Loading tortoise......ok\n")
---
+++ run.scm
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+guile -L ./ -l init.scm
